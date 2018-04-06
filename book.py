85a40fd18a0366c26509d1292f92c185ab1b2bbc
ywang <yong.wang@murex.com>
2018-04-06 15:17:26 +0800

    email feature

--- book.py
+++ book.py
@@ -2,4 +2,3 @@ class Book():
     book_name = u''
     book_auther = u''
     book_url = ''
-    book_chapter_urls = []
--- download_book.py
+++ download_book.py
@@ -4,32 +4,34 @@ from bs4 import BeautifulSoup
 import json
 import re
 import xml.etree.ElementTree as ET
+import operator
+from collections import OrderedDict
 from book import Book
 
+
 def search_book(bookname):
-    url = 'https://www.googleapis.com/customsearch/v1element'
+    url = 'https://content.googleapis.com/customsearch/v1?'
     payload = {
-        'key' : 'AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY',
-        'num':'1',
-        'hl':'zh_CN',
+        'key' : 'AIzaSyDR9xsxHcem0B-HyaOZ9pOLeKsBCluD9N0',
         'cx':'004126777754148953309:ebpsbzqi5le',
-        'q': bookname,
-        'cse_tok':'ABPF6Hg1cVl9gzESFzu_qB8L1T5fuhjLiA:1522749215678'
+        'q': bookname
         }
 
     headers ={}
     cookies = {}
 
-    r = requests.get(url, headers=headers, params=payload, cookies=cookies)
+    r = requests.get(url, headers=headers, params=payload, cookies=cookies, timeout=5)
+
+    #print r.url
 
     soup = BeautifulSoup(r.content,'html.parser')
 
     json_result = json.loads(str(soup))
-    if len(json_result['results']) > 0:
+    if len(json_result['items']) > 0:
         this_book = Book()
         this_book.book_name = bookname
-        this_book.book_url = json_result['results'][0]['url']
-
+        this_book.book_url = json_result['items'][0]['formattedUrl']
+        print this_book.book_url
         r = requests.get(this_book.book_url )
         soup = BeautifulSoup(r.text, 'html.parser')
 
@@ -50,20 +52,23 @@ def download_book(bookname, url):
     book_number = url_parer[-2]
 
     #get all page urls
-    page_urls = []
+    page_details = OrderedDict()
 
     for chapter in soup.find_all(target='_blank'):
         if 'b/'+str(book_number) in chapter['href']:
-            if chapter['href'] not in page_urls:
-                page_urls.append(chapter['href'])
+            page_key = int(str(chapter['href']).split('/')[-1].split('.')[0])
+            page_url = chapter['href']
+            page_title = chapter.text
+            page_details[page_key]=[page_url,page_title]
 
-    page_urls = sorted(page_urls)
+    page_details=OrderedDict(sorted(page_details.items(),key=lambda t:t[0]))
 
     book_content = ''
 
-    for page_url in page_urls:
-        actual_page_url = url+str(re.split("/",page_url)[-1])
-        #book_content = book_content + get_one_page(actual_page_url)
+    for key , value in page_details.iteritems():
+        actual_page_url = url+str(re.split("/",value[0])[-1])
+        print 'Download book chapter url: ' + actual_page_url
+        book_content = book_content + get_one_page(actual_page_url)
 
     return book_content
 
@@ -104,13 +109,18 @@ def dump_book(bookname, conent):
     f.close()
 
 
-def update_book(url):
+def update_book(book):
     pass
 
-def monitor_book(url):
+def monitor_book(book):
+    pass
+
+def email_updated_page(book):
     pass
 
 bookname = u'诡秘之主'
 searched_book = search_book(bookname)
 book_content = download_book(bookname, searched_book.book_url)
 dump_book(bookname, book_content)
+
+
